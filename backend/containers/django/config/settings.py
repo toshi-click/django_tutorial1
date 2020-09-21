dfc1963f4024eb83caa6e80b9dffee45cc9ae552
toshi <toshi@toshi.click>
2020-09-21 15:35:18 +0900

    アプリケーションを拡張しよう部

--- backend/containers/django/config/settings.py
+++ backend/containers/django/config/settings.py
@@ -116,9 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
+# #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'blog/static/'),
+)
 #REST_FRAMEWORK = {
 #    'DEFAULT_AUTHENTICATION_CLASSES': [
 #        'rest_framework_simplejwt.authentication.JWTAuthentication',
